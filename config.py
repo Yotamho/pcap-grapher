df7b27c944cb9744977302ba5182c6f79808142a
yotamh <yotam.hochman@cyberbit.net>
2017-09-15 11:24:00 +0300

    Edit and elaborate the argparse config module

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import argparse
 
-parser = argparse.ArgumentParser(description="create schema to a PCAP")
-parser.add_argument("pcap", metavar="p", help="path to pcap file")
-parser.add_argument("client_ip", metavar="c", help="client's ip")
-parser.add_argument("-f", dest="display_filter", metavar="f", help="display filter")
+parser = argparse.ArgumentParser(description="Create an intuitive and interactive graph of a client's IP traffic")
+parser.add_argument("pcap_path", metavar="<pcap path>", help="path to the pcap file")
+parser.add_argument("client_ip", metavar="<client's ip>",
+                    help="client's ip allow filtering and focusing on a specific IP")
+parser.add_argument("-f", dest="display_filter", metavar="display-filter",
+                    help="Wireshark display filter, additional to the client filter.")
 
 args = parser.parse_args()
-pcap_path = args.pcap
+pcap_path = args.pcap_path
 display_filter = args.display_filter
 client_ip = args.client_ip
